36711eec2b7028f41d8dfd7b5f3e8ea3c9f4acff
adm1 <guiraudjb@gmail.com>
2026-05-26 18:15:21 +0200

    init wpa

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'arducible-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json'
+];
+
+// Installation : Mise en cache des ressources
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Récupération : Servir depuis le cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
